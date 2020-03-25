f1ac6c7c7442f82ecaddbbe9ca62f0e5d693989b
soumik-mukherjee <me@soumikmukherjee.dev>
2020-03-25 18:44:04 +0530

    Code change iteration#1 with basic DevTools extension testing OK

--- components/Redux/store.js
+++ components/Redux/store.js
@@ -7,19 +7,22 @@
  */
 
 // Dependencies
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose  } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 //import storage from "redux-persist/lib/storage/session";
 // Reducers
 import rootReducer from './reducers'
 
+// Compose enhancer including the DevTools extension
+const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // Create the Redux store.
 
 const store =
   process.env.NODE_ENV === 'production'
     ? createStore(rootReducer, applyMiddleware(thunk))
-    : createStore(rootReducer, applyMiddleware(thunk, logger))
+    : createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk, logger)))
 
 // Export the Redux store.
 export { store }
